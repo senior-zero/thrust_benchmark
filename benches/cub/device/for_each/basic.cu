4cb5ae1fff4fed9d6e4f72521ff7f397d144f527
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2022-04-17 22:29:38 +0400

    Vary load modifiers

--- benches/cub/device/for_each/basic.cu
+++ benches/cub/device/for_each/basic.cu
@@ -23,10 +23,12 @@ struct select_op
 
 
 template <typename T,
-          cub::ForEachAlgorithm Algorithm>
+          cub::ForEachAlgorithm Algorithm,
+          cub::CacheLoadModifier LoadModifier>
 static void basic(nvbench::state &state,
                   nvbench::type_list<T, 
-                                     nvbench::enum_type<Algorithm>>)
+                                     nvbench::enum_type<Algorithm>,
+                                     nvbench::enum_type<LoadModifier>>)
 {
   const auto elements = static_cast<std::size_t>(state.get_int64("Elements"));
 
@@ -39,7 +41,7 @@ static void basic(nvbench::state &state,
   state.add_element_count(elements);
   state.add_global_memory_reads(elements * sizeof(T));
 
-  auto tuning = cub::TuneForEach<Algorithm>(
+  auto tuning = cub::TuneForEach<Algorithm, LoadModifier>(
     cub::ForEachConfigurationSpace{}.Add<256, 4>());
 
   state.exec([&](nvbench::launch &launch) {
@@ -61,6 +63,11 @@ using algorithms = nvbench::enum_type_list<cub::ForEachAlgorithm::BLOCK_STRIPED,
                                            cub::ForEachAlgorithm::VECTORIZED>;
 
 
+using modifiers = nvbench::enum_type_list<cub::CacheLoadModifier::LOAD_DEFAULT,
+                                          cub::CacheLoadModifier::LOAD_CA,
+                                          cub::CacheLoadModifier::LOAD_CS>;
+
+
 NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
   cub::ForEachAlgorithm,
   [](cub::ForEachAlgorithm algorithm) {
@@ -73,7 +80,7 @@ NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
       default:
         break;
     }
-    NVBENCH_THROW(std::runtime_error, "{}", "Unknown data_pattern");
+    NVBENCH_THROW(std::runtime_error, "{}", "Unknown algorithm");
   },
   [](cub::ForEachAlgorithm algorithm) {
     switch (algorithm)
@@ -85,12 +92,60 @@ NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
       default:
         break;
     }
-    NVBENCH_THROW(std::runtime_error, "{}", "Unknown data_pattern");
+    NVBENCH_THROW(std::runtime_error, "{}", "Unknown algorithm");
+  })
+
+
+NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
+  cub::CacheLoadModifier,
+  [](cub::CacheLoadModifier modifier) {
+    switch (modifier)
+    {
+      case cub::CacheLoadModifier::LOAD_DEFAULT:
+        return "LOAD_DEFAULT";
+      case cub::CacheLoadModifier::LOAD_CA:
+        return "LOAD_CA";
+      case cub::CacheLoadModifier::LOAD_CG:
+        return "LOAD_CG";
+      case cub::CacheLoadModifier::LOAD_CS:
+        return "LOAD_CS";
+      case cub::CacheLoadModifier::LOAD_CV:
+        return "LOAD_CV";
+      case cub::CacheLoadModifier::LOAD_LDG:
+        return "LOAD_LDG";
+      case cub::CacheLoadModifier::LOAD_VOLATILE:
+        return "LOAD_VOLATILE";
+      default:
+        break;
+    }
+    NVBENCH_THROW(std::runtime_error, "{}", "Unknown load modifier");
+  },
+  [](cub::CacheLoadModifier modifier) {
+    switch (modifier)
+    {
+      case cub::CacheLoadModifier::LOAD_DEFAULT:
+        return "Default (no modifier)";
+      case cub::CacheLoadModifier::LOAD_CA:
+        return "Cache at all levels";
+      case cub::CacheLoadModifier::LOAD_CG:
+        return "Cache at global level";
+      case cub::CacheLoadModifier::LOAD_CS:
+        return "Cache streaming (likely to be accessed once)";
+      case cub::CacheLoadModifier::LOAD_CV:
+        return "Cache as volatile (including cached system lines)";
+      case cub::CacheLoadModifier::LOAD_LDG:
+        return "Cache as texture";
+      case cub::CacheLoadModifier::LOAD_VOLATILE:
+        return "Volatile (any memory space)";
+      default:
+        break;
+    }
+    NVBENCH_THROW(std::runtime_error, "{}", "Unknown load modifier");
   })
 
 
 NVBENCH_BENCH_TYPES(basic,
-                    NVBENCH_TYPE_AXES(types, algorithms))
+                    NVBENCH_TYPE_AXES(types, algorithms, modifiers))
   .set_name("cub::DeviceFor::ForEachN")
   .add_int64_power_of_two_axis("Elements", nvbench::range(16, 28, 2));
 
