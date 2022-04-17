712b2f1756a12d72023df07d78c887a002d85fb9
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2022-04-17 22:12:58 +0400

    Vary ForEach algorithms

--- benches/cub/device/for_each/basic.cu
+++ benches/cub/device/for_each/basic.cu
@@ -1,12 +1,11 @@
 #include <nvbench/nvbench.cuh>
+#include <nvbench/detail/throw.cuh>
 
-#include <thrust/count.h>
 #include <thrust/device_vector.h>
-#include <thrust/execution_policy.h>
-#include <thrust/sequence.h>
 #include <thrust/detail/raw_pointer_cast.h>
 
 #include <cub/device/device_for.cuh>
+
 
 template <typename T>
 struct select_op
@@ -22,9 +21,12 @@ struct select_op
   }
 };
 
-template <typename T>
+
+template <typename T,
+          cub::ForEachAlgorithm Algorithm>
 static void basic(nvbench::state &state,
-                  nvbench::type_list<T>)
+                  nvbench::type_list<T, 
+                                     nvbench::enum_type<Algorithm>>)
 {
   const auto elements = static_cast<std::size_t>(state.get_int64("Elements"));
 
@@ -37,16 +39,58 @@ static void basic(nvbench::state &state,
   state.add_element_count(elements);
   state.add_global_memory_reads(elements * sizeof(T));
 
+  auto tuning = cub::TuneForEach<Algorithm>(
+    cub::ForEachConfigurationSpace{}.Add<256, 4>());
+
   state.exec([&](nvbench::launch &launch) {
-    cub::DeviceFor::ForEachN(d_input, elements, select_op<T>{d_counter});
+    cub::DeviceFor::ForEachN(d_input,
+                             elements,
+                             select_op<T>{d_counter},
+                             launch.get_stream(),
+                             false,
+                             tuning);
   });
 }
 
+
 using types =
-  nvbench::type_list<nvbench::uint32_t, nvbench::uint64_t>;
+  nvbench::type_list<nvbench::uint32_t>;
+
+
+using algorithms = nvbench::enum_type_list<cub::ForEachAlgorithm::BLOCK_STRIPED,
+                                           cub::ForEachAlgorithm::VECTORIZED>;
+
+
+NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
+  cub::ForEachAlgorithm,
+  [](cub::ForEachAlgorithm algorithm) {
+    switch (algorithm)
+    {
+      case cub::ForEachAlgorithm::BLOCK_STRIPED:
+        return "BLOCK_STRIPED";
+      case cub::ForEachAlgorithm::VECTORIZED:
+        return "VECTORIZED";
+      default:
+        break;
+    }
+    NVBENCH_THROW(std::runtime_error, "{}", "Unknown data_pattern");
+  },
+  [](cub::ForEachAlgorithm algorithm) {
+    switch (algorithm)
+    {
+      case cub::ForEachAlgorithm::BLOCK_STRIPED:
+        return "Single element per load - block striped";
+      case cub::ForEachAlgorithm::VECTORIZED:
+        return "Four elements per load - block striped";
+      default:
+        break;
+    }
+    NVBENCH_THROW(std::runtime_error, "{}", "Unknown data_pattern");
+  })
+
 
 NVBENCH_BENCH_TYPES(basic,
-                    NVBENCH_TYPE_AXES(types))
+                    NVBENCH_TYPE_AXES(types, algorithms))
   .set_name("cub::DeviceFor::ForEachN")
   .add_int64_power_of_two_axis("Elements", nvbench::range(16, 28, 2));
 
